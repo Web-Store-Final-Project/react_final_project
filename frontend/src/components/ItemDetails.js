5203eab90c886cc23dfc3a26d807e7936a830baf
gal levy <gallevy34@gmail.com>
2022-12-22 18:32:41 +0200

    added frontend folder
    show the items from db

---
+++ frontend/src/components/ItemDetails.js
@@ -0,0 +1,13 @@
+
+const ItemDetails = ({item})=>{
+    return(
+        <div className="item-details">
+            <h4>{item.title}</h4>
+            <p><strong>Description: </strong>{item.description}</p>
+            <p><strong>Price: </strong>{item.price}$</p>
+            <img src= {item.imgPath}></img>
+        </div> 
+    );
+}
+
+export default ItemDetails
