8a580c5014f075b7348724d509e746c763001039
Roy Ambar <93988278+royambar1@users.noreply.github.com>
2022-12-28 15:00:45 +0200

    updated page organization

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,35 +2,33 @@ import { useEffect } from "react";
 import { useItemsContext } from "../hooks/useItemsContext";
 
 //components
-import ItemDetails from '../components/ItemDetails'
-import ItemForm from "../components/ItemForm";
+import ItemDetails from "../components/ItemDetails";
+// import ItemForm from "../components/ItemForm";
 
-const Home = ()=>{
-    const {items,dispatch} = useItemsContext()
+const Home = () => {
+  const { items, dispatch } = useItemsContext();
 
-    useEffect(()=>{
-        const fetchItems = async ()=>{
-            const response = await fetch('/api/items')
-            const json = await response.json()
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch("/api/items");
+      const json = await response.json();
 
-            if(response.ok){
-                dispatch({type:'SET_ITEMS',payload:json})
-            }
-        }
+      if (response.ok) {
+        dispatch({ type: "SET_ITEMS", payload: json });
+      }
+    };
 
-        fetchItems()
-    }, [dispatch])
-    
-    return(
-        <div className="home">
-            <div className="items">
-                {items && items.map(item=>(
-                    <ItemDetails item = {item} key={item._id}/>
-                ))}
-            </div>
-            <ItemForm/>
-        </div>
-    );
-}
+    fetchItems();
+  }, [dispatch]);
 
-export default Home
+  return (
+    <div className="home">
+      <div className="items">
+        {items &&
+          items.map((item) => <ItemDetails item={item} key={item._id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
