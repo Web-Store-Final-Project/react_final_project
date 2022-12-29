db93e4daed838ed96349ef38188c730d43927c30
gal levy <gallevy34@gmail.com>
2022-12-29 15:06:37 +0200

    new folder + file for scraping

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
-const axios = require('axios');
 const itemRoutes = require('./routes/itemsRout.js')
 
-const axios = require('axios')
-const cheerio = require('cheerio')
+const scraper  = require( './scrapers/scrapeBurton.js')
 
 // express app
 const app = express()
@@ -20,66 +19,9 @@ app.use((req,res,next)=>{
 
 //routes
 app.use('/api/items' ,itemRoutes)
-app.use('/api/users' ,userRoutes)
 
-const url = 'https://www.burton.com/us/en/c/mens-snowboards?start=0&sz=24'
-
-const itemsArr = []
-
-axios(url).then(response =>{
-    const html =  response.data
-    const $ =  cheerio.load(html)
-
-    $('.product-tile' ,html).each(function(){
-        const title = $(this).find('.product-name').text()
-        const price = $(this).find('.standard-price').text()
-        const imgSrc = $(this).find('.product-image').attr('src')
-        const urlDescription = $(this).find('a').attr('href')
-
-        if(title !== "" && price !== "" && imgSrc !== ""){
-            itemsArr.push({
-                title,
-                price,
-                imgSrc,
-                urlDescription
-            })
-        }
-    })
-
-    console.log(itemsArr)
-    
-}).catch(err => console.log(err))
-
-
-
-//scraping from burton
-const url = 'https://www.burton.com/us/en/c/mens-apparel-accessories?start=0&sz=24'
-const itemsArr = []
-
-axios(url).then(response =>{
-    const html =  response.data
-    const $ =  cheerio.load(html)
-
-    $('.product-tile' ,html).each(function(){
-        const title = $(this).find('.product-name').text()
-        const price = $(this).find('.standard-price').text()
-        const imgSrc = $(this).find('.product-image').attr('src')
-        // const urlDescription = $(this).find('a').attr('href')
-
-        if(title !== "" && price !== "" && imgSrc !== ""){
-            itemsArr.push({
-                title,
-                price,
-                imgSrc
-                // urlDescription
-            })
-        }
-    })   
-    console.log(itemsArr)
-    const data = JSON.stringify(itemsArr)
-
-}).catch(err=>console.log(err))
-    
+//scrape
+scraper.apply()
 
 //connect to db
 mongoose.set('strictQuery',true);
